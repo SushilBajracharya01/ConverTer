95e5e688234e42faace6b28bc4106148ac83c272
SushilBajracharya01 <Bajracharya.sushil01@gmail.com>
2017-01-15 20:06:17 +0545

    Create Binary-Converter.c

---
+++ Binary-Converter.c
@@ -0,0 +1,22 @@
+// a program to convert decimal number into binary.
+
+#include <stdio.h>
+#include<conio.h>
+
+int main(){
+int anyNum,mod,div;
+printf("Enter a decimal number : ");
+scanf("%d",&anyNum);
+div=anyNum;
+int sum=0;
+int mul = 1;
+do{
+    mod=div%2;
+    div=div/2;
+    sum=sum+mod*mul;
+    mul=mul*10;
+}while(div>0);
+printf("The binary naumber for %d is %d",anyNum,sum);
+return 0;
+}
+    
